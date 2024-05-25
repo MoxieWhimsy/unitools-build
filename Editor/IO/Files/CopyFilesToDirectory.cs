e39ef6097b9ca89ec4cf2ab4d7e62aa42112fadf
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2024-05-25 00:26:58 -0400

    Added source directory property

--- Editor/IO/Files/CopyFilesToDirectory.cs
+++ Editor/IO/Files/CopyFilesToDirectory.cs
@@ -11,23 +11,33 @@ namespace UniTools.Build
     )]
     public sealed class CopyFilesToDirectory : BuildStep
     {
+        [SerializeField] private PathProperty m_sourceDirectory = default;
         [SerializeField] private PathProperty[] m_sourceFiles = default;
         [SerializeField] private PathProperty m_destinationDirectory = default;
 
         public override async Task Execute()
         {
+            var sourceFolder = m_sourceDirectory.ToString();
+            var hasNoSourceFolder = string.IsNullOrEmpty(sourceFolder);
             var destinationFolder = m_destinationDirectory.ToString();
+
+            if (!hasNoSourceFolder && !Directory.Exists(sourceFolder))
+            {
+                throw new DirectoryNotFoundException($"Source folder: {sourceFolder} does not exist!");
+            }
             if (!Directory.Exists(destinationFolder))
             {
-                throw new DirectoryNotFoundException($"{destinationFolder} does not exist!");
+                throw new DirectoryNotFoundException($"Destination folder {destinationFolder} does not exist!");
             }
             
-            foreach (var filePath in m_sourceFiles)
+            foreach (var sourceFile in m_sourceFiles)
             {
-                var fileName = Path.GetFileName(filePath.ToString());
-                var dest = Path.Combine(destinationFolder, fileName);
-                FileUtil.DeleteFileOrDirectory(dest);
-                FileUtil.CopyFileOrDirectory(filePath.ToString(), dest);
+                var filePath = sourceFile.ToString();
+                var fileName = Path.GetFileName(filePath);
+                var source = hasNoSourceFolder ? filePath : Path.Combine(sourceFolder, filePath);
+                var destination = Path.Combine(destinationFolder, fileName);
+                FileUtil.DeleteFileOrDirectory(destination);
+                FileUtil.CopyFileOrDirectory(source, destination);
             }
             
             await Task.CompletedTask;
